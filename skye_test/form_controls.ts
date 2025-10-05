e6fcea2bba434117b55bc85e5a982ef8e921a7b3
9128hotel <skyekaplynvasquez@gmail.com>
2025-10-05 12:52:29 +0100

    new file:   skye_test/focusable.ts modified:   skye_test/form_controls.ts

--- skye_test/form_controls.ts
+++ skye_test/form_controls.ts
@@ -1,8 +1,8 @@
 function test_form_controls(elements: Document) {
     const forms = elements.querySelectorAll<HTMLFormElement>("form")
-    var badForms: HTMLFormElement[] = []
+    var badForms: (HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement | HTMLButtonElement)[] = []
     for (const form of forms) {
-        const labels = form.querySelectorAll("label")
+
         const otherElements = [
             ...Array.from(form.querySelectorAll<HTMLInputElement>("input")),
             ...Array.from(form.querySelectorAll<HTMLSelectElement>("select")),
@@ -10,8 +10,21 @@ function test_form_controls(elements: Document) {
             ...Array.from(form.querySelectorAll<HTMLButtonElement>("button")),
         ];
 
-        if (labels.length < otherElements.length) {
-            badForms.concat(form)
+        const visibleOtherElements = otherElements.filter(c => !(c instanceof HTMLInputElement && c.type === "hidden"))
+
+        for (const element of visibleOtherElements) {
+            // wrapped by or referenced by label
+            const id = (element as Element).id
+            const hasForLabel = id ? !!form.querySelector(`label[for="${CSS.escape(id)}"]`) : false
+            const isWrappedByLabel = !!element.closest("label")
+
+            const ariaLabelledBy = element.getAttribute("aria-labelledby")
+            const hasAriaLabelledBy = !!ariaLabelledBy && Array.from(form.querySelectorAll(`#${ariaLabelledBy.split(/\s+/).map(s => CSS.escape(s)).join(",#")}`)).length > 0 // element has ariaLabeledBy and the element its referencing exists within the form
+            const ariaLabel = element.getAttribute("aria-label")
+
+            const hasLabel = hasForLabel || isWrappedByLabel || hasAriaLabelledBy || (!!ariaLabel && ariaLabel.trim().length > 0)
+
+            if (!hasLabel) {badForms.push(element)}
         }
 
         // warn - not implemented yet
