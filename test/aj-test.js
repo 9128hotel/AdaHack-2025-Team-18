fc5b65db1f585aa6e8b4a2c62e3f46229a91e9d9
lonely-little-astronaut <annabeljwade@gmail.com>
2025-10-05 11:18:33 +0100

    modified:   test/aj-test.js

--- test/aj-test.js
+++ test/aj-test.js
@@ -2,9 +2,11 @@ function imgsWithoutAlts() {
     const imgs = document.querySelectorAll('img');
     var badIDs = []
     for (const img of imgs) {
-        if (img.alt.length() < 15){
+        if (img.alt.length < 15){
             badIDs.push(img.id)
         }
     }
     return badIDs
 }
+
+
