08198c16e3883311609814987051f823b4a9fef6
9128hotel <skyekaplynvasquez@gmail.com>
2025-10-05 11:58:12 +0100

    new file:   skye_test/form_controls.ts

---
+++ skye_test/form_controls.ts
@@ -0,0 +1,22 @@
+function test_form_controls(elements: Document) {
+    const forms = elements.querySelectorAll<HTMLFormElement>("form")
+    var badForms: HTMLFormElement[] = []
+    for (const form of forms) {
+        const labels = form.querySelectorAll("label")
+        const otherElements = [
+            ...Array.from(form.querySelectorAll<HTMLInputElement>("input")),
+            ...Array.from(form.querySelectorAll<HTMLSelectElement>("select")),
+            ...Array.from(form.querySelectorAll<HTMLTextAreaElement>("textarea")),
+            ...Array.from(form.querySelectorAll<HTMLButtonElement>("button")),
+        ];
+
+        if (labels.length < otherElements.length) {
+            badForms.concat(form)
+        }
+
+        // warn - not implemented yet
+        
+    }
+
+    return badForms
+}
