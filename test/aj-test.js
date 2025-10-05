c7da418855b24b283a3452d6961cb5123ee4b886
lonely-little-astronaut <annabeljwade@gmail.com>
2025-10-05 11:10:09 +0100

    new file:   test/aj-test.js

---
+++ test/aj-test.js
@@ -0,0 +1,10 @@
+function imgsWithoutAlts() {
+    const imgs = document.querySelectorAll('img');
+    var badIDs = []
+    for (const img of imgs) {
+        if (img.alt.length() < 15){
+            badIDs.push(img.id)
+        }
+    }
+    return badIDs
+}
